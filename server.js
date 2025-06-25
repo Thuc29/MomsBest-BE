b5c55dc779a0af5a838663d8e5e3f13a597ac195
ducnq <ducnqhe173294@fpt.edu.vn>
2025-06-25 16:40:18 +0700

    update cors 2

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ adminUserController.createDefaultAdmin();
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://momsbest-fe.onrender.com/"],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    origin: ["http://localhost:3000", "https://momsbest-fe.onrender.com"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: [
       "Content-Type",
       "Authorization",
