12ccebce725e6ac38f79ebaba56adfff3c0fcd88
Thuc29 <112091905+Thuc29@users.noreply.github.com>
2025-07-03 14:32:55 +0700

    update catPro

--- routers/product.js
+++ routers/product.js
@@ -1,24 +1,26 @@
 const express = require("express");
 const Product = require("../model/Product");
 const router = express.Router();
+const categoryProductController = require("../controller/admin/categoryProduct");
+router.get("/categories", categoryProductController.getAll);
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const products = await Product.find()
+    const products = await Product.find();
     res.status(200).json(products);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-})
+});
 
-router.get('/:productId', async (req, res) => {
+router.get("/:productId", async (req, res) => {
   try {
-    const { productId } = req.params
-    const product = await Product.findById(productId)
+    const { productId } = req.params;
+    const product = await Product.findById(productId);
     res.status(200).json(product);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-})
+});
 
 module.exports = router;
